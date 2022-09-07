5355b51ae12bd5bead94c204c3d738b636a612a9
shahrizschool <shahrizschool@gmail.com>
2022-09-07 15:07:31 -0700

    Everythings runs through one terminal and am testing aws

--- applications/client/src/App.js
+++ applications/client/src/App.js
@@ -31,7 +31,7 @@ function App() {
   return (
     <div className="App">
         <div className="usersDisplay">
-          {listOfUsers.map((user) => {
+          {listOfUsers.map((user) => { 
             return (
             <div>
               <h1> Name: {user.name} </h1>
