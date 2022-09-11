27ddcc47077698a9530aa010a2655aa7581d7d63
Mandy Noto <mandynoto@icloud.com>
2022-09-11 12:06:32 -0700

    feat(client): add mandy pic, name, and role

--- applications/client/src/pages/People/Mandy.js
+++ applications/client/src/pages/People/Mandy.js
@@ -4,8 +4,8 @@ function Mandy() {
     return (
       <div>
         <h1>This is the about Mandy </h1>
-        <h1>Name: </h1>
-        <h1>Role: </h1>
+        <h1>Name: Mandy Noto </h1>
+        <h1>Role: GitHub Master </h1>
         <img src="logo192.png" />
       </div>
     );
