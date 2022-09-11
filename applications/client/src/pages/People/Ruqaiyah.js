cc6f43d2717d6cf816a41879dfadc589a32f2495
Ruqaiyah22 <kaya4152201#gmail.com>
2022-09-11 11:31:09 -0700

    finished my about me page

--- applications/client/src/pages/People/Ruqaiyah.js
+++ applications/client/src/pages/People/Ruqaiyah.js
@@ -4,8 +4,8 @@ function Ruqaiyah() {
     return (
       <div className="person">
         <h1>This is the about Ruqaiyah </h1>
-        <h1>Name: </h1>
-        <h1>Role: </h1>
+        <h1>Name: Ruqaiyah Angeles</h1>
+        <h1>Role: Team Lead</h1>
         <img src="logo192.png" />
        
       </div>
