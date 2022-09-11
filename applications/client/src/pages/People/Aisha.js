f99962244577cd9e7714911f2f1eabce6996958d
aishaasifm <aishasif.m@gmail.com>
2022-09-11 12:38:33 -0700

    added picture

--- applications/client/src/pages/People/Aisha.js
+++ applications/client/src/pages/People/Aisha.js
@@ -4,8 +4,8 @@ function Aisha() {
     return (
       <div>
         <h1>This is the about Aisha </h1>
-        <h1>Name: </h1>
-        <h1>Role: </h1>
+        <h1>Name: Aisha Asif </h1>
+        <h1>Role: Scrum Master </h1>
         <img src="logo192.png" />
       </div>
     );
