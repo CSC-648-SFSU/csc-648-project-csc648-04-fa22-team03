6e511b25287ce40a154e10ed715ffe7006f1419d
Ahmed Ballal <71238322+aballal-source@users.noreply.github.com>
2022-09-11 19:23:34 -0700

    style
    Updated Ahmed's about me page and added an image

--- applications/client/src/pages/People/Ahmed.js
+++ applications/client/src/pages/People/Ahmed.js
@@ -4,8 +4,8 @@ function Ahmed() {
     return (
       <div>
         <h1>This is the about Ahmed </h1>
-        <h1>Name: </h1>
-        <h1>Role: </h1>
+        <h1>Name: Ahmed Ballal</h1>
+        <h1>Role: SWE Helper</h1>
         <img src="logo192.png" alt="Ahmed"/>
       </div>
     );
