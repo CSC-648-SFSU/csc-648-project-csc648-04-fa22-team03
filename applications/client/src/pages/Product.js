07a897eb52d4f4282543eea1c907e7cf1bcc9191
Chris Solorzano <chrisxsolo177@gmail.com>
2022-11-02 13:56:19 -0700

    feat(pages): Product Page Routing

---
+++ applications/client/src/pages/Product.js
@@ -0,0 +1,21 @@
+
+import "./components/ProductCss.css";
+import { Route, Routes } from "react-router-dom"
+import ProductLink from "./components/ProductLink";
+
+
+
+
+export default function Product() {
+
+    return (
+        <>
+            <div className="cont">
+                <ProductLink />  
+                <Routes>
+                 
+                </Routes>
+            </div>
+        </>
+    )
+}
