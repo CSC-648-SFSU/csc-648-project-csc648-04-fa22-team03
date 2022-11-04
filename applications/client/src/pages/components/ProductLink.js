5e2ba45373c1f658a9ea3412fb7c93b386414044
Chris Solorzano <chrisxsolo177@gmail.com>
2022-11-04 15:32:38 -0700

    feat(components) Products Page

--- applications/client/src/pages/components/ProductLink.js
+++ applications/client/src/pages/components/ProductLink.js
@@ -1,14 +1,35 @@
 import { Link, useMatch, useResolvedPath } from "react-router-dom"
-
-
+import React from "react"
 
 export default function ProductLink() {
     return (
         <div className="boutdiv">
             <h1> Product Page</h1>
-            <ul>
-               
-            </ul>
+            <section>
+		<div class="main-container">
+			<ul class="grid-wrapper">
+				<li class = "box" id = "box-01">
+                    <div class = "product-title">
+                    <CustomLink to="/Awake">Awake </CustomLink>
+                    </div>
+                    <div class = "caption">Mitigates the efects of sleep deprivation</div>
+                </li>
+				<li class = "box" id = "box-02"> 
+                <div class = "product-title">
+                <CustomLink to="/Relax">Relax </CustomLink>
+                </div>
+                <div class = "caption">Comforts when around strangers</div>
+                </li>
+				<li class = "box" id = "box-03">  
+                <div class = "product-title">
+                    <CustomLink to="/Neutralize">Neutralize </CustomLink>
+                    </div>
+                <div class = "caption">Neutralize ones own smell</div>
+                </li>
+
+			</ul>
+		</div>
+	</section>
         </div>
     )
 }
