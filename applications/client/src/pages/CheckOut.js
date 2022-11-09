45f542bb22f51fd95950aaee2e874efd83067c70
Chris Solorzano <chrisxsolo177@gmail.com>
2022-11-08 18:07:50 -0800

    feat(applicatons): created skeleton of checkout page and routing

---
+++ applications/client/src/pages/CheckOut.js
@@ -0,0 +1,20 @@
+import "./components/CheckOut.css";
+import { Route, Routes } from "react-router-dom"
+import CheckOutLink from "./components/CheckOutLink";
+
+
+
+
+export default function CheckOut(){
+
+    return (
+        <>
+            <div className="cont">
+                <CheckOutLink />  
+                <Routes>
+             
+                </Routes>
+            </div>
+        </>   
+    )
+}
